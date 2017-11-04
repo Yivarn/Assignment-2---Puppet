562e583b3661408f3e4939eb2bd75a3f3acee078
Yivarn Norman <s3627699@student.rmit.edu.au>
2017-11-04 16:46:14 +0000

    Split apache installer out of the packages module into apache.pp.
    For cross combatability the apache installer was separated out as a standalone module.
    Apache is called httpd on fedora and redhat systems, whereas debain uses apache2.
    The site.pp file was modified to declare apache.pp.

---
+++ apache.pp
@@ -0,0 +1,12 @@
+class apache{
+  $distname = $::operatingsystem ? {
+    'RedHat' => 'httpd',
+    'Fedora' => 'httpd',	
+    'Debian' => 'apache2',
+  }
+
+  package { 'apache':
+    ensure => 'latest',
+    name => $distname,
+  }
+}
