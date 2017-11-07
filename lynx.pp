3cf2527fec5d8acc4c3d45b9ac59ed366c736ad6
Yivarn Norman <s3627699@student.rmit.edu.au>
2017-11-07 12:46:12 +0000

    Forgot to update lynx.pp and packages.pp

--- lynx.pp
+++ lynx.pp
@@ -8,7 +8,7 @@ class lynx{
 
       package { 'lynx':
         ensure => 'latest',
-        require => Exec['yum-config'], # require the yum-config exec before the package can be installed
+        before => Exec['yum-config'], # require the yum-config exec before the package can be installed
       }
     }
 
--- packages.pp
+++ packages.pp
@@ -1,41 +1,15 @@
-# Class: packages
-# ===========================
-#
-# This class manages the instalation of required packages.
-#
-# Author
-# ------
-#
-# Yivarn Norman <s362769@student.rmit.edu.au>
-#
 class packages {
 	Package{ ensure => 'installed' } 
-
 	package { 'openssh': }
-
-	# apache
-	#package { 'httpd': }
-
 	#package { 'mysql-client': }
 	#package { 'mysql-server': }
-
-	# vnc-server
-	#package { 'tigervnc-server': }
-
 	#package { 'tmux': }
 	#package { 'dia2code': }
-
-	# yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional 
-	#package { 'lynx': }
-
 	package { 'gcc': }
 	package { 'gdb': }
-	
 	#package { 'cgdb': }
-	
 	package { 'vim': }
 	package { 'emacs': }
-
 	#package { 'sshfs': }
 }
 
