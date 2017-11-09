0882ee4155a7924982081f90b23ed0d53925fcd4
Yivarn Norman <s3627699@student.rmit.edu.au>
2017-11-09 04:54:39 +0000

    Finally fixed hiera-eyaml installation.
    Hiera-eyaml seems to require two installations to work.
    Much messing around was required to finally get this version to work.
    
    Not currently using hiera.pp hierainstall.pp or gem.pp not sure if still required.

--- hieraeyaml.pp
+++ hieraeyaml.pp
@@ -1,7 +1,20 @@
-class hieraeyaml { 
-  exec { 'install hiera-eyaml': 
-    command => "puppetserver gem install hiera-eyaml",
-    unless => "puppetserver gem list --local | grep hiera-eyaml",
-    path => ['/usr/bin', '/opt/puppetlabs/bin'],
+class hieraeyaml {
+
+  exec { 'gem install hiera-eyaml':
+    path => ['/opt/puppetlabs/puppet/bin','/bin'],
+    creates => '/opt/puppetlabs/puppet/bin/eyaml',
   }
+
+  exec { 'puppetserver gem install hiera-eyaml':
+    path => ['/opt/puppetlabs/bin','/bin','/sbin'],
+    creates => '/opt/puppetlabs/server/data/puppetserver/jruby-gems/bin/eyaml',
+  }
+
+  exec { 'createkeys':
+    command => 'eyaml createkeys',
+    path => ['/opt/puppetlabs/puppet/bin/','/bin/'],
+    creates => '/opt/puppetlabs/puppet/bin/keys/private_key.pkcs7.pem',
+  }
+
+
 }
