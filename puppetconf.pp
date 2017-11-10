56c611e12ab67f4e6e18699ebe37d277e7fd13b8
Yivarn Norman <s3627699@student.rmit.edu.au>
2017-11-10 18:19:58 +0000

    Updated puppetconf.pp to ensure puppet refreshes appropriately.
    puppetconf.pp now ensures the puppet service is running,
    and that it is refreshed if the puppet.conf file is changed.

--- puppetconf.pp
+++ puppetconf.pp
@@ -4,4 +4,13 @@ class puppetconf {
     context => "/files/etc/puppetlabs/puppet/puppet.conf/agent", # path to file
     changes => 'set runinterval 1190', # set ~19.8 minutes between runs
   }
+
+  service { 'puppet':
+    ensure => 'running',
+    enable => true,
+  }
+
+  file { '/etc/puppetlabs/puppet/puppet.conf':
+    notify=> Service['puppet'],
+  }
 }
