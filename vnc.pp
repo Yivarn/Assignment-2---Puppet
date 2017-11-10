751f3e8fdb0b680e6e1c08270a17172aaba67594
Yivarn Norman <s3627699@student.rmit.edu.au>
2017-11-10 20:22:41 +0000

    Modified vnc.pp to start managing vnc config setup (not yet complete).
    Currently copies template file post installation to appropriate position.
    Then providing the service is not running, meaning that the installation
    was already successful, it edits the config file for root user.
    
    Still need to reload the daemon, initialize the password, enable
    the service and start/manage the service (including config subscription).

--- vnc.pp
+++ vnc.pp
@@ -1,11 +1,46 @@
 class vnc{
-  $distname = $::operatingsystem ? {
-    'RedHat' => 'tigervnc-server',
-    'Debian' => 'vnc-server',
-  }
 
-  package { 'vnc':
-    ensure => 'latest',
-    name => $distname,
+  case $::operatingsystem { # switch depending on OS
+
+    'RedHat': { # if the operating system is RedHat
+
+      package { 'tigervnc-server':
+        ensure => 'latest',
+      }
+
+      exec { 'setup_vnc_1':
+        command => 'cp /lib/systemd/system/vncserver@.service /etc/systemd/system',
+        creates => '/etc/systemd/system/vncserver@.service',
+        path => '/bin/', # the path to cp
+        require => Package['tigervnc-server'],
+      }
+
+      exec { 'setup_vnc_2':
+        cwd => '/etc/systemd/system/',
+        command => "sed -i 's/<USER>/root/' vncserver@.service",
+        path => '/bin/', # the path to sed
+        require => Exec['setup_vnc_1'],
+        unless => '/sbin/service vncserver status',
+      }
+
+      exec { 'setup_vnc_3':
+        cwd => '/etc/systemd/system/',
+        command => "sed -i 's#PIDFile=/home#PIDFile=#' vncserver@.service",
+        path => '/bin/', # the path to sed
+        require => Exec['setup_vnc_2'],
+        unless => '/sbin/service vncserver status',
+      }
+
+      
+
+    }
+
+    'Debian':{ # alternative for Debian
+      package { 'vnc':
+        ensure => 'latest',
+      }
+    }
+
   }
 }
+
